e2a27719ad7124ae38a6459e7e884828ddc89f75
indigorose <indigorose049@gmail.com>
2025-09-08 23:16:07 +0100

    Implement basic CRUD operations for habits and update requirements

--- habit-tracker-backend/app/main.py
+++ habit-tracker-backend/app/main.py
@@ -15,3 +15,37 @@ def read_root():
 @app.get("/habits")
 def get_habits():
     return habits
+
+
+# POST endpoint(create habit)
+@app.post("/habits")
+def create_habit(habit: Habit):
+    habits.append(habit)
+
+
+# get POST by ID
+@app.get("/habits/{habit_id}")
+def get_habit(habit_id: int):
+    for habit in habits:
+        if habit.id == habit_id:
+            return habit
+    return {"error": "Habit not found"}
+
+
+# PUT - update a habit
+@app.put("/habits/{habit_id}")
+def update_habit(habit_id: int, updated_habit: Habit):
+    for i, h in enumerate(habits):
+        if h.id == habit_id:
+            habits[i] = updated_habit
+            return updated_habit
+    return {"error": "Habit not found"}
+
+
+# Delete a post
+@app.delete("/habits/{habit_id}")
+def delete_habit(habit_id: int):
+    for i, h in enumerate(habits):
+        if h.id == habit_id:
+            return habits.pop(i)
+    return {"error": "Habit not found"}
